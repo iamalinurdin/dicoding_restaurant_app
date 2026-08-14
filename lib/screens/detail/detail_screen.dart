fb1351c41bab213c54c55f4f4d3beb55409d8932
iamalinurdin <alinurdin.dev@gmail.com>
2026-08-15 04:06:44 +0700

    fix

--- lib/screens/detail/detail_screen.dart
+++ lib/screens/detail/detail_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:restaurant_app/data/http/api_service.dart';
+import 'package:restaurant_app/data/models/restaurant_detail.dart';
 import 'package:restaurant_app/data/models/restaurant_detail_response.dart';
 
 class DetailScreen extends StatefulWidget {
@@ -39,35 +40,34 @@ class _DetailScreenState extends State<DetailScreen> {
 
               final restaurant = snapshot.data!.restaurant;
 
-              return Column(
-                children: [
-                  Stack(
-                    children: [
-                      ClipRRect(
-                        borderRadius: .only(
-                          bottomLeft: .circular(20),
-                          bottomRight: .circular(20),
+              return SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Stack(
+                      children: [
+                        ClipRRect(
+                          borderRadius: .only(
+                            bottomLeft: .circular(20),
+                            bottomRight: .circular(20),
+                          ),
+                          child: Image.network(
+                            "https://restaurant-api.dicoding.dev/images/large/${restaurant.pictureId}",
+                          ),
                         ),
-                        child: Image.network(
-                          "https://restaurant-api.dicoding.dev/images/large/${restaurant.pictureId}",
-                        ),
-                      ),
-                      Positioned(
-                        top: 50,
-                        left: 20,
+                        Positioned(
+                          top: 50,
+                          left: 20,
 
-                        child: IconButton(
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                          icon: Icon(Icons.arrow_back, color: Colors.white),
+                          child: IconButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            icon: Icon(Icons.arrow_back, color: Colors.white),
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
+                      ],
+                    ),
+                    Column(
                       crossAxisAlignment: .start,
                       children: [
                         Row(
@@ -87,34 +87,213 @@ class _DetailScreenState extends State<DetailScreen> {
                             ),
                           ],
                         ),
-                        SizedBox(height: 10),
+                        SizedBox(height: 12),
                         Row(
                           children: [
                             Icon(Icons.location_pin, size: 16),
                             Text(restaurant.address),
                           ],
                         ),
-                        SizedBox(height: 10),
+                        SizedBox(height: 12),
                         Text(
                           'Description',
-                          style: Theme.of(context).textTheme.titleMedium,
+                          style: Theme.of(
+                            context,
+                          ).textTheme.titleMedium!.copyWith(fontWeight: .w500),
                         ),
                         SizedBox(height: 4),
                         Text(
                           restaurant.description,
                           style: Theme.of(context).textTheme.labelMedium!
-                              .copyWith(color: Colors.black, fontWeight: .w500),
+                              .copyWith(color: Colors.black, fontWeight: .w400),
                         ),
+                        SizedBox(height: 12),
+                        MenusListView(restaurant: restaurant),
+                        SizedBox(height: 12),
+                        Text(
+                          'Review',
+                          style: Theme.of(
+                            context,
+                          ).textTheme.titleMedium!.copyWith(fontWeight: .w500),
+                        ),
+                        SizedBox(
+                          height: 100,
+                          child: ListView.separated(
+                            scrollDirection: .horizontal,
+                            itemCount: restaurant.customerReviews.length,
+                            separatorBuilder: (context, index) {
+                              return const SizedBox(width: 12);
+                            },
+                            itemBuilder: (context, index) {
+                              return ConstrainedBox(
+                                constraints: BoxConstraints(
+                                  maxWidth: 300,
+                                  maxHeight: 150,
+                                  minHeight: 150,
+                                  minWidth: 200,
+                                ),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    border: Border.all(
+                                      color: Colors.black87,
+                                      width: 1,
+                                    ),
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Column(
+                                      crossAxisAlignment: .start,
+                                      mainAxisAlignment: .spaceBetween,
+                                      children: [
+                                        Text(
+                                          restaurant
+                                              .customerReviews[index]
+                                              .review,
+                                          style: Theme.of(
+                                            context,
+                                          ).textTheme.titleMedium,
+                                          maxLines: 3,
+                                        ),
+                                        Row(
+                                          mainAxisAlignment: .spaceBetween,
+                                          children: [
+                                            Text(
+                                              restaurant
+                                                  .customerReviews[index]
+                                                  .name,
+                                              style: Theme.of(
+                                                context,
+                                              ).textTheme.labelSmall,
+                                            ),
+                                            Text(
+                                              restaurant
+                                                  .customerReviews[index]
+                                                  .date,
+                                              style: Theme.of(
+                                                context,
+                                              ).textTheme.labelSmall,
+                                            ),
+                                          ],
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+                        ),
+                        SizedBox(height: 50),
                       ],
                     ),
-                  ),
-                ],
+                  ],
+                ),
               );
             default:
               return SizedBox();
           }
         },
       ),
+    );
+  }
+}
+
+class MenusListView extends StatelessWidget {
+  const MenusListView({super.key, required this.restaurant});
+
+  final RestaurantDetail restaurant;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: .start,
+      children: [
+        Text(
+          'Foods',
+          style: Theme.of(
+            context,
+          ).textTheme.titleMedium!.copyWith(fontWeight: .w500),
+        ),
+        SizedBox(height: 4),
+        SizedBox(
+          height: 100,
+          child: ListView.separated(
+            scrollDirection: Axis.horizontal,
+            itemCount: restaurant.menus.foods.length,
+            separatorBuilder: (context, index) {
+              return const SizedBox(width: 12);
+            },
+            itemBuilder: (context, index) {
+              return SizedBox(
+                width: 200,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: Stack(
+                    fit: StackFit.expand,
+                    children: [
+                      Image.asset('assets/images/foods.jpg', fit: BoxFit.cover),
+                      Positioned(
+                        bottom: 10,
+                        left: 8,
+                        child: Text(
+                          restaurant.menus.foods[index].name,
+                          style: Theme.of(context).textTheme.labelLarge!
+                              .copyWith(fontWeight: FontWeight.w600),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+        SizedBox(height: 12),
+        Text(
+          'Drinks',
+          style: Theme.of(
+            context,
+          ).textTheme.titleMedium!.copyWith(fontWeight: .w500),
+        ),
+        SizedBox(height: 4),
+        SizedBox(
+          height: 100,
+          child: ListView.separated(
+            scrollDirection: Axis.horizontal,
+            itemCount: restaurant.menus.drinks.length,
+            separatorBuilder: (context, index) {
+              return const SizedBox(width: 12);
+            },
+            itemBuilder: (context, index) {
+              return SizedBox(
+                width: 200,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: Stack(
+                    fit: StackFit.expand,
+                    children: [
+                      Image.asset(
+                        'assets/images/drinks.jpg',
+                        fit: BoxFit.cover,
+                      ),
+                      Positioned(
+                        bottom: 10,
+                        left: 8,
+                        child: Text(
+                          restaurant.menus.drinks[index].name,
+                          style: Theme.of(context).textTheme.labelLarge!
+                              .copyWith(fontWeight: FontWeight.w600),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ],
     );
   }
 }
